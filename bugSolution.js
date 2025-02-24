f0e74be03f1a74b6d05a122276c8fc93a3b85b4a
BugHunter-X <askzenai@gmail.com>
2025-02-24 12:10:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* The solution: add a wildcard to prevent unexpected matching */}
+        <Route path="/contact/*" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {return <div>Home</div>}
+function About() {return <div>About</div>}
+function Contact() {return <div>Contact</div>} 
